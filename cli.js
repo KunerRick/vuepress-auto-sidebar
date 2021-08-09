b2486310101f9ca4baca1b2afd2bcd8aa1b74160
KunerRick <821393625@qq.com>
2021-08-09 19:27:34 +0800

    因与主题插件冲突，增加判断条件

--- cli.js
+++ cli.js
@@ -1,33 +1,44 @@
 #!/usr/bin/env node
-const fs = require("fs");
-const path = require("path");
-const { getRootDir, getSidebar } = require("./common");
+const fs = require('fs')
+const path = require('path')
+const { getRootDir, getSidebar } = require('./common')
 
-let baseOption = undefined;
+let baseOption = undefined
 
-(function() {
+;(function () {
   // 参数为指定的文档父文件夹，如不指定，就默认从cwd 开始遍历
-  const argv = process.argv;
-  let dir = [];
+  const argv = process.argv
+  let dir = []
   if (argv.length > 2) {
-    dir = argv.slice(2);
+    dir = argv.slice(2)
   }
-  let sidebar = getSidebar(process.cwd(), dir);
-  rewriteConfig(sidebar);
-  console.log("config rewriting completed...");
-})();
+  let sidebar = getSidebar(process.cwd(), dir)
+  rewriteConfig(sidebar)
+  console.log('config rewriting completed...')
+})()
 
 function rewriteConfig(sidebar) {
-  let config = require(path.join(getRootDir(), "/.vuepress/config.js"));
-  config.themeConfig.sidebar = sidebar;
-  let content = getConfigFileContent();
-  let match = content.match(/(?<=themeConfig: )(.(.|\r\n|\r|\n)*)/);
-  let output = JSON.stringify(config.themeConfig, null, 4);
-  let newContent = content.substring(0, match.index) + output + "\r\n}";
-  fs.writeFileSync(path.join(getRootDir(), "/.vuepress/config.js"), newContent);
+  let config = require(path.join(getRootDir(), '/.vuepress/config.js'))
+
+  config.themeConfig.sidebar = sidebar
+  let content = getConfigFileContent()
+  let match = content.match(/(?<=themeConfig: )(.(.|\r\n|\r|\n)*)/)
+
+  let output = JSON.stringify(config.themeConfig, null, 4)
+
+  let endStr = ''
+
+  // 适配函数类型的导出
+  if (content.indexOf('config({') !== -1) {
+    endStr = '\r\n})'
+  } else {
+    endStr = '\r\n}'
+  }
+  let newContent = content.substring(0, match.index) + output + endStr
+  fs.writeFileSync(path.join(getRootDir(), '/.vuepress/config.js'), newContent)
 }
 
 function getConfigFileContent() {
-  let configFile = path.join(getRootDir(), "/.vuepress/config.js");
-  return fs.readFileSync(configFile, "utf8");
+  let configFile = path.join(getRootDir(), '/.vuepress/config.js')
+  return fs.readFileSync(configFile, 'utf8')
 }
